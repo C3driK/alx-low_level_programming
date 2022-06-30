58be6d186138cc63155307d1f45d10c92d006348
Femoski <olufemiphilips@outlook.com>
2022-06-30 13:51:13 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+
+/*
+*main - Entry point
+*print word to output
+*Return: Always 0 (Success)
+*/
+
 int main(void)
-
-
 {
-char Mystr[] = "Programming is like building a multilingual puzzle)";
-
-puts(Mystr);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
